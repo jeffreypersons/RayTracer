7cbc378429b8feae407d0faa754389140391764d
Jeffrey Persons <jperman8@gmail.com>
2021-01-21 09:42:48 -0800

    Standardize timer class verbiage to distinguish exception-throwing messages from traditional getters

--- src/Timer.hpp
+++ src/Timer.hpp
@@ -33,19 +33,19 @@ public:
 
     bool isRunning()  const { return startTime.has_value() && !stopTime.has_value(); }
     bool isFinished() const { return startTime.has_value() && stopTime.has_value();  }
-    double getStartTime() const {
+    double timeStarted() const {
         if (!startTime.has_value()) {
             throw std::runtime_error("startTime undefined - requires call to start() after initialization or clear()");
         }
         return startTime.value();
     }
-    double getStopTime() const {
+    double timeStopped() const {
         if (!stopTime.has_value()) {
             throw std::runtime_error("stopTime undefined - computed in stop() after calling start()");
         }
         return stopTime.value();
     }
-    double elapsedTime() const {
+    double timeElapsed() const {
         if (isRunning()) {
             return duration(startTime.value(), currentTime().value());
         }
