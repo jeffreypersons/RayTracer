f7c78513d1f9c06918068c5c7d8b4bb724ec6279
Jeffrey Persons <jperman8@gmail.com>
2021-01-28 13:31:02 -0800

    Create simplified main for testing

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 
-RenderCam createFrontalSceneViewCam(const Vec3& position, float fieldOfView, float viewDist) {
+RenderCam createFrontCam(const Vec3& position, float fieldOfView, float viewDist) {
     RenderCam cam{};
     cam.setPosition(position);
     cam.setNearClip(viewDist);
@@ -14,53 +14,23 @@ RenderCam createFrontalSceneViewCam(const Vec3& position, float fieldOfView, flo
     cam.setOrientation(Vec3::right(), Vec3::up(), Vec3::ahead());
     return cam;
 }
-RenderCam createBottomUpSceneViewCam(const Vec3& position, float fieldOfView, float viewDist) {
-    RenderCam cam{};
-    cam.setPosition(position);
-    cam.setNearClip(viewDist);
-    cam.setFarClip(10000.00f);
-    cam.setAspectRatio(1.00f);
-    cam.setFieldOfView(fieldOfView);
-    cam.setOrientation(Vec3::ahead(), Vec3::right(), Vec3::up());
-    return cam;
-}
 
 Scene createSimpleScene() {
     Material brightWhite{};
     brightWhite.setColors(Palette::gray, 0.75 * Palette::white, Palette::white);
 
-    Material matteBlue{};
-    matteBlue.setWeights(1.00f, 0.00f);
-    matteBlue.setAmbientColor(Palette::blue);
-    matteBlue.setDiffuseColor(Palette::blue);
-    matteBlue.setSpecularColor(Palette::white);
-
     Material reflectiveGreen{};
     reflectiveGreen.setWeights(0.50f, 0.50f);
     reflectiveGreen.setDiffuseColor(Palette::green);
-
-    Material shinyRed{};
-    shinyRed.setWeights(0.95f, 0.05f);
-    shinyRed.setSpecularColor(Palette::red);
-
-    Scene scene{};
-    scene.addLight(Light(Vec3(0, 55, -25), brightWhite));
-    //scene.addSceneObject(Sphere(Vec3(  0, -1500,  0), 1000, matteBlue));
-    scene.addSceneObject(Sphere(Vec3(-25,    35, 10),    5, reflectiveGreen));
-    scene.addSceneObject(Sphere(Vec3(  0,    50,  0),   20, reflectiveGreen));
-    scene.addSceneObject(Sphere(Vec3(  0,    20,  0),    5, shinyRed));
     
-    std::cout << "\n\nmatte-blue-"     << matteBlue       << "\n";
-    std::cout << "\nreflective-green-" << reflectiveGreen << "\n";
-    std::cout << "\nshiny-red-"        << shinyRed        << "\n\n";
-
+    Scene scene{};
+    scene.addLight(Light(Vec3(0, 55, 0), brightWhite));
+    scene.addSceneObject(Sphere(Vec3(0, 35, 15), 20, reflectiveGreen));
     return scene;
 }
 
 int main()
 {
-    StopWatch stopWatch{};
-    stopWatch.start();
     std::cout << "Program started...\n";
 
     Tracer tracer{};
@@ -71,25 +41,16 @@ int main()
     tracer.setMinTForShadowIntersections(0.01f);
 
     FrameBuffer frameBuffer(Vec2(1250, 1250), Palette::skyBlue);
-    Scene simpleScene   = createSimpleScene();
-    RenderCam frontCam  = createFrontalSceneViewCam( Vec3(0, 40, 50), 90.00f, 5.00f);
-    RenderCam bottomCam = createBottomUpSceneViewCam(Vec3(0, 10,  0), 90.00f, 5.00f);
+    Scene scene   = createSimpleScene();
+    RenderCam cam = createFrontCam( Vec3(0, 40, 50), 90.00f, 5.00f);
 
-    std::cout << "Initializing target buffer..\n  " << frameBuffer << "\n\n";
-    std::cout << "Assembling scene..\n.."           << simpleScene << "\n\n";
-    std::cout << "Configuring frontal and bottom-up view cams\n  " << frontCam << "\n  " << bottomCam << "\n\n";
+    std::cout << "Assembling scene..\n.." << scene << "\n\n";
+    std::cout << "Configuring frontal-view cams\n  " << cam << "\n\n";
 
-    std::cout << "Rendering scene from multiple perspectives..\n";
-    tracer.trace(frontCam, simpleScene, frameBuffer);
-    frameBuffer.writeToFile("./scene_front-view");
+    tracer.trace(cam, scene, frameBuffer);
+    frameBuffer.writeToFile("./scene");
     std::cout << "  wrote rendered scene as image output to file '" << "./scene_front-view" << "'\n";
 
-    tracer.trace(bottomCam, simpleScene, frameBuffer);
-    frameBuffer.writeToFile("./scene_bottom-view");
-    std::cout << "  wrote rendered scene as image output to file '" << "./scene_bottom-view" << "'\n\n";
-    
-    stopWatch.stop();
-    std::cout << "Program finished in " << stopWatch.elapsedTime() << " seconds\n";
     std::cout << "Press ENTER to end...";
     std::cin.get();
 }
