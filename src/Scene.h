c868aa1e7139b9071303fe1734d8c3c683a865b4
Jeffrey Persons <jperman8@gmail.com>
2021-02-18 16:17:43 -0700

    Extract definitions to Scene.cpp

---
+++ src/Scene.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Lights.h"
+#include "Objects.h"
+#include <vector>
+
+
+// simple container for accessing the lights and objects in the order they were added
+// currently only supports indexed access, no custom iterators (yet)
+class Scene {
+private:
+    std::vector<std::unique_ptr<ILight>> lights;
+    std::vector<std::unique_ptr<IObject>> objects;
+
+public:
+    Scene();
+
+    void addLight(PointLight light);
+    void addSceneObject(Sphere object);
+    void addSceneObject(Triangle object);
+
+    size_t getNumLights() const;
+    size_t getNumObjects() const;
+    const ILight& getLight(size_t index) const;
+    const IObject& getObject(size_t index) const;
+};
+std::ostream& operator<<(std::ostream& os, const Scene& scene);
