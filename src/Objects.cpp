80d38b5626164de2e890c1888aa35e7f6fe1d85b
Jeffrey Persons <jperman8@gmail.com>
2021-02-19 12:41:58 -0700

    Objects: Change all fields to end in underscore and user names for accessors

--- src/Objects.cpp
+++ src/Objects.cpp
@@ -6,18 +6,20 @@
 // assumes radius greater than zero
 Sphere::Sphere(const Vec3& center, float radius, const Material& material) {
     // note that base class members can only be initialized in the body and not our typical initializer list
-    this->centroid = center;
-    this->material = material;
-    this->radius   = radius;
+    this->radius_ = radius;
+    this->center_ = center;
+
+    this->position_ = center;
+    this->material_ = material;
 }
 
 // given ray [X(t) = P + t] intersects sphere [| X – C | = r] IFF there exists
 // at least one real number t [t = - D.M +/- sqrt((D.M)^2 – ( |M|^2 – r^2 ))]
 // (that is, if above discriminant >= 0, then there exists a point along ray that also lies along sphere's surface)
 bool Sphere::intersect(const Ray& ray, Intersection& result) const {
-    Vec3 M = ray.origin - this->centroid;
+    Vec3 M = ray.origin - this->position_;
     float dDotM = Math::dot(ray.direction, M);
-    float discriminant = (dDotM * dDotM) - (Math::magnitudeSquared(M) - radius * radius);
+    float discriminant = (dDotM * dDotM) - (Math::magnitudeSquared(M) - radius_ * radius_);
     if (discriminant < 0.00f) {
         return false;
     }
@@ -32,21 +34,24 @@ bool Sphere::intersect(const Ray& ray, Intersection& result) const {
     }
     result.t      = closestT;
     result.point  = ray.origin + ray.direction * result.t;
-    result.normal = (result.point - this->centroid) / this->radius;
+    result.normal = (result.point - this->position_) / this->radius_;
     result.object = this;
     return true;
 }
 
-float Sphere::getRadius() const {
-    return radius;
+constexpr Vec3 Sphere::center() const {
+    return center_;
+}
+constexpr float Sphere::radius() const {
+    return radius_;
 }
 
-std::string Sphere::getDescription() const {
+std::string Sphere::description() const {
     std::stringstream ss;
     ss << "Sphere("
-         << "centroid:(" << getCentroid() << "),"
-         << "material:"  << getMaterial() << ","
-         << "radius:"    << getRadius()
+         << "position:(" << center()   << "),"
+         << "material:"  << material() << ","
+         << "radius:"    << radius()
        << ")";
     return ss.str();
 }
@@ -54,68 +59,74 @@ std::string Sphere::getDescription() const {
 
 Triangle::Triangle(const Vec3& vert0, const Vec3& vert1, const Vec3& vert2, const Material& material) {
     // note that base class members can only be initialized in the body and not our typical initializer list
-    this->material = material;
-    this->vert0 = vert0;
-    this->vert1 = vert1;
-    this->vert2 = vert2;
-    this->edge0 = vert1 - vert0;
-    this->edge1 = vert2 - vert1;
-    this->edge2 = vert0 - vert2;
-    this->centroid    = computeCentroid();
-    this->planeNormal = Math::normalize(Math::cross(edge0, edge1));
+    this->vert0_ = vert0;
+    this->vert1_ = vert1;
+    this->vert2_ = vert2;
+    this->edge0_ = vert1 - vert0;
+    this->edge1_ = vert2 - vert1;
+    this->edge2_ = vert0 - vert2;
+    this->planeNormal_ = Math::normalize(Math::cross(edge0_, edge1_));
+    this->center_ = computeCentroid();
+
+    this->material_ = material;
+    this->position_ = this->center_;
 }
 
 // given ray intersects triangle IFF given ray [X(t) = P + tD] intersects the plane [P.n = k] in a way such that
 // our intersection point is always to the LEFT side of EVERY edge
 // (aka our plane intersection point @[t = (k – P.n)/(D.n)] lies in between our triangle vertices)
 bool Triangle::intersect(const Ray& ray, Intersection& result) const {
-    float k = Math::dot(vert0, planeNormal);
-    float t = (k - Math::dot(ray.origin, planeNormal)) / Math::dot(ray.direction, planeNormal);
+    float k = Math::dot(vert0_, planeNormal_);
+    float t = (k - Math::dot(ray.origin, planeNormal_)) / Math::dot(ray.direction, planeNormal_);
     Vec3 pointIntersectingPlane = ray.origin + ray.direction * t;
-    if (isPointInTriangle(pointIntersectingPlane)) {
+    if (contains(pointIntersectingPlane)) {
         result.t      = t;
         result.point  = pointIntersectingPlane;
-        result.normal = planeNormal;
+        result.normal = planeNormal_;
         result.object = this;
         return true;
     }
     return false;
 }
 
-Vec3 Triangle::computeCentroid() const {
-    return Vec3((vert0.x + vert1.x + vert2.x) / 3.00f,
-                (vert0.y + vert1.y + vert2.y) / 3.00f,
-                (vert0.z + vert1.z + vert2.z) / 3.00f);
+constexpr Vec3 Triangle::vert0() const {
+    return vert0_;
 }
+constexpr Vec3 Triangle::vert1() const {
+    return vert1_;
+}
+constexpr Vec3 Triangle::vert2() const {
+    return vert2_;
+}
+constexpr Vec3 Triangle::planeNormal() const {
+    return planeNormal_;
+}
+constexpr Vec3 Triangle::center() const {
+    return center_;
+}
+
 // recall, a point lies in a triangle if..
 // (e0 x(R – P0)).n > 0 & (e1 x(R – P1)).n > 0 & (e2 x(R – P2)).n > 0
 // aka R is always to the LEFT side of EVERY edge
-bool Triangle::isPointInTriangle(const Vec3& point) const {
-    return (Math::dot(planeNormal, Math::cross(edge0, (point - vert0))) > 0.00f) &&
-           (Math::dot(planeNormal, Math::cross(edge1, (point - vert1))) > 0.00f) &&
-           (Math::dot(planeNormal, Math::cross(edge2, (point - vert2))) > 0.00f);
+constexpr bool Triangle::contains(const Vec3& point) const {
+    return (Math::dot(planeNormal_, Math::cross(edge0_, (point - vert0_))) > 0.00f) &&
+           (Math::dot(planeNormal_, Math::cross(edge1_, (point - vert1_))) > 0.00f) &&
+           (Math::dot(planeNormal_, Math::cross(edge2_, (point - vert2_))) > 0.00f);
 }
 
-Vec3 Triangle::getVert0() const {
-    return vert0;
-}
-Vec3 Triangle::getVert1() const {
-    return vert1;
-}
-Vec3 Triangle::getVert2() const {
-    return vert2;
-}
-Vec3 Triangle::getPlaneNormal() const {
-    return planeNormal;
+constexpr Vec3 Triangle::computeCentroid() const {
+    return Vec3((vert0_.x + vert1_.x + vert2_.x) / 3.00f,
+                (vert0_.y + vert1_.y + vert2_.y) / 3.00f,
+                (vert0_.z + vert1_.z + vert2_.z) / 3.00f);
 }
 
-std::string Triangle::getDescription() const {
+std::string Triangle::description() const {
     std::stringstream ss;
     ss << "Triangle("
-         << "centroid:("        << getCentroid()    << "),"
-         << "plane-normal:(v0:" << getPlaneNormal() << "),"
-         << "material:"         << getMaterial()    << ","
-         << "verts:[v0:(" << getVert0() << "),v1:(" << getVert1() << "),v2:(" << getVert2() << ")]"
+         << "position:("        << center()      << "),"
+         << "plane-normal:(v0:" << planeNormal() << "),"
+         << "material:"         << material()    << ","
+         << "verts:[v0:(" << vert0() << "),v1:(" << vert1() << "),v2:(" << vert2() << ")]"
        << ")";
     return ss.str();
 }
--- src/Objects.h
+++ src/Objects.h
@@ -7,58 +7,62 @@
 // virtual base class for ANY renderable (via ray-tracing) object in a scene
 class IObject {
 protected:
-    Vec3 centroid{};
-    Material material{};
+    Vec3 position_{};
+    Material material_{};
 
 public:
     ~IObject() {}
 
     virtual bool intersect(const Ray& ray, Intersection& result) const = 0;
-    virtual std::string getDescription() const = 0;
-
-    Vec3     getCentroid() const { return centroid; }
-    Material getMaterial() const { return material; }
+    virtual std::string description() const = 0;
+    
+    constexpr Vec3 position() const  { return position_; }
+    constexpr const Material& material() const { return material_; }
 };
 inline std::ostream& operator<<(std::ostream& os, const IObject& object) {
-    os << object.getDescription();
+    os << object.description();
     return os;
 }
 
 
 class Sphere : public IObject {
 private:
-    float radius;
+    float radius_;
+    Vec3 center_;
 
 public:
     Sphere(const Vec3& center, float radius, const Material& material);
     virtual bool intersect(const Ray& ray, Intersection& result) const override;
-    virtual std::string getDescription() const override;
-
-    float getRadius() const;
+    virtual std::string description() const override;
+    
+    constexpr float radius() const;
+    constexpr Vec3 center()  const;
 };
 
 
 class Triangle : public IObject {
 private:
-    Vec3 vert0;
-    Vec3 vert1;
-    Vec3 vert2;
-    Vec3 edge0;
-    Vec3 edge1;
-    Vec3 edge2;
-    Vec3 planeNormal;
-    
+    Vec3 vert0_;
+    Vec3 vert1_;
+    Vec3 vert2_;
+    Vec3 edge0_;
+    Vec3 edge1_;
+    Vec3 edge2_;
+    Vec3 planeNormal_;
+    Vec3 center_;
+
 public:
     Triangle(const Vec3& vert0, const Vec3& vert1, const Vec3& vert2, const Material& material);
     virtual bool intersect(const Ray& ray, Intersection& result) const override;
-    virtual std::string getDescription() const override;
-
-    Vec3 getVert0()       const;
-    Vec3 getVert1()       const;
-    Vec3 getVert2()       const;
-    Vec3 getPlaneNormal() const;
+    virtual std::string description() const override;
+    
+    constexpr Vec3 vert0()       const;
+    constexpr Vec3 vert1()       const;
+    constexpr Vec3 vert2()       const;
+    constexpr Vec3 planeNormal() const;
+    constexpr Vec3 center()      const;
+    constexpr bool contains(const Vec3& point) const;
 
 private:
-    Vec3 computeCentroid() const;
-    bool isPointInTriangle(const Vec3& point) const;
+    constexpr Vec3 computeCentroid() const;
 };
