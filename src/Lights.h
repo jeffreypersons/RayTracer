57b6af1685ce0e2390da2344e96e175178ccf575
Jeffrey Persons <jperman8@gmail.com>
2021-03-12 13:46:04 -0800

    Lights: Order externally usable methods first, with public constexprs inlined

--- src/Lights.h
+++ src/Lights.h
@@ -13,8 +13,8 @@ public:
     virtual Color computeIntensityAtPoint(const Vec3& point) const = 0;
     virtual std::string description() const = 0;
 
-    constexpr Vec3  position()  const { return position_;  }
-    constexpr Color intensity() const { return intensity_; }
+    constexpr const Vec3&  position()  const { return position_;  }
+    constexpr const Color& intensity() const { return intensity_; }
 };
 inline std::ostream& operator<<(std::ostream& os, const ILight& light) {
     os << light.description();
@@ -23,21 +23,25 @@ inline std::ostream& operator<<(std::ostream& os, const ILight& light) {
 
 
 class PointLight final : public virtual ILight {
-private:
-    float attenuationConstant_;
-    float attenuationLinear_;
-    float attenuationQuadratic_;
-
 public:
-    PointLight(const Vec3& position, const Color& intensity, float constant, float linear, float quadratic);
     PointLight(const Vec3& position, const Color& intensity);
+    PointLight(const Vec3& position, const Color& intensity, float constant, float linear, float quadratic);
 
     virtual Color computeIntensityAtPoint(const Vec3& point) const override;
     virtual std::string description() const override;
 
+    float attenuationConstant()  const;
+    float attenuationLinear()    const;
+    float attenuationQuadratic() const;
+
     void setAttenuation(float constant, float linear, float quadratic);
 
-    constexpr float attenuationConstant()  const;
-    constexpr float attenuationLinear()    const;
-    constexpr float attenuationQuadratic() const;
+private:
+    float attenuationConstant_;
+    float attenuationLinear_;
+    float attenuationQuadratic_;
+    
+    static constexpr float DEFAULT_ATTENUATION_CONSTANT  = 1.00f;
+    static constexpr float DEFAULT_ATTENUATION_LINEAR    = 0.00f;
+    static constexpr float DEFAULT_ATTENUATION_QUADRATIC = 0.00f;
 };
