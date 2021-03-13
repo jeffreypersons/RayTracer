af3d8e441d46bc41969d82cc735f957816f60c78
Jeffrey Persons <jperman8@gmail.com>
2021-03-12 16:54:46 -0800

    FrameBuffer: Define common resolutions adjacent to buffer class

--- src/FrameBuffer.hpp
+++ src/FrameBuffer.hpp
@@ -7,6 +7,20 @@
 #include <assert.h>
 
 
+namespace CommonResolutions {
+    inline constexpr Vec2 SD_240p  = Vec2(  426,  240);
+    inline constexpr Vec2 SD_360p  = Vec2(  640,  360);
+    inline constexpr Vec2 SD_480p  = Vec2(  854,  480);
+    inline constexpr Vec2 HD_720p  = Vec2( 1280,  720);
+    inline constexpr Vec2 HD_1080p = Vec2( 1920, 1080);
+    inline constexpr Vec2 HD_2K    = Vec2( 2560, 1440);
+    inline constexpr Vec2 HD_4K    = Vec2( 3840, 2160);
+    inline constexpr Vec2 HD_5K    = Vec2( 5120, 2880);
+    inline constexpr Vec2 HD_8K    = Vec2( 7680, 4320);
+    inline constexpr Vec2 HD_10K   = Vec2(10328, 7760);
+    inline constexpr Vec2 HD_12K   = Vec2(12288, 6480);
+}
+
 class FrameBuffer {
 private:
     size_t width_;
