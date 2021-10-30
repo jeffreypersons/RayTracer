5ac912eb14b4bde3226cc6627655d7e9695d404c
Jeffrey Persons <jperman8@gmail.com>
2021-10-30 08:32:37 -0400

    Scene: Make header format consistent

--- src/Scene.h
+++ src/Scene.h
@@ -14,13 +14,14 @@ public:
     void addSceneObject(Sphere&& object);
     void addSceneObject(Triangle&& object);
 
-    const ILight&  getLight(size_t index) const;
+    const ILight&  getLight(size_t index)  const;
     const IObject& getObject(size_t index) const;
-    size_t getNumLights() const;
+
+    size_t getNumLights()  const;
     size_t getNumObjects() const;
 
 private:
-    std::vector<std::unique_ptr<ILight>> lights_;
+    std::vector<std::unique_ptr<ILight>>  lights_;
     std::vector<std::unique_ptr<IObject>> objects_;
 };
 std::ostream& operator<<(std::ostream& os, const Scene& scene);
